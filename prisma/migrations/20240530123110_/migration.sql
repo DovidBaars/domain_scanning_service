905eb20233eb116fc2a5a7980f55c02647ffad35
Dovid Baars <65543628+DovidBaars@users.noreply.github.com>
2024-05-30 17:38:50 +0300

    remove migrations

--- prisma/migrations/20240530123110_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[domainId,scanApiId]` on the table `Results` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Results_domainId_scanApiId_key" ON "Results"("domainId", "scanApiId");
